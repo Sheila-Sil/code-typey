703f5952d86e42d54541a1ed502f886d36dae1b0
Sheila Sil <89608834+Sheila-Sil@users.noreply.github.com>
2026-08-19 09:56:27 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "./",
+import { defineConfig } from 'vite'
+import react from '@vitejs/react-refresh' // or your framework plugin
+
+export default defineConfig({
+  plugins: [react()],
+  base: 'code-typey', 
+})
+
 });
